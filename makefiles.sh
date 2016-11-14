b0319c505bf137a5c9909aabcead3c6fe1491c23
Murray Tipping <mjt@tippings.net>
2016-11-13 20:00:12 -0500

    added find command so that only the dotfiles are symlinked

--- makefiles.sh
+++ makefiles.sh
@@ -2,19 +2,23 @@
 ##
 ## makefiles.sh
 ##
-## uses git to clone files from Github repo into a directory ~/dotfiles, moves existing files into a hidden directory .dotfiles_old, then symlinks the new files to the proper place.
+## To be run after cloning dotfiles from repo.
+## Moves existing dotfiles into a hidden directory .dotfiles_old, then symlinks the new files to the proper place.
 
 dir=~/dotfiles	#dotfiles directory
 olddir=~/.dotfiles_old	#old dotfiles directory
-
+files="$(find . -maxdepth 1 -type f  ! '(' -name 'LICENSE' -o -name '*.sh' ')'  -exec basename {} \;)"
 ### create .dotfiles_old
 
 echo "Creating $olddir to backup any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done!"
 
-#clone my dotfiles from the Github repo
+#move any existing dotfile in homedir to dotfiles_old, then create symlinks
 
-echo "cloning files from Github"
-git clone 
-
+for file in $files; do
+	echo "Moving old dotfiles from ~ to $olddir"
+	mv ~/.$file ~/.dotfiles_old/
+	echo "creating symlinks to $file in home directory..."
+	ln -s $dir/$file ~/.$file
+done
