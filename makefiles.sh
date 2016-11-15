f38566effd9e50c12b21e71004213211e7b810f0
gibbslab2 <gibbslab2@dhcp-140-247-106-172.fas.harvard.edu>
2016-11-15 17:03:25 -0500

    empties backup dir if exists, else creates it

--- makefiles.sh
+++ makefiles.sh
@@ -10,15 +10,20 @@ olddir=~/.dotfiles_old	#old dotfiles directory
 files="$(find . -maxdepth 1 -type f  ! '(' -name 'LICENSE' -o -name '*.sh' ')'  -exec basename {} \;)"
 ### create .dotfiles_old
 
-echo "Creating $olddir to backup any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating empty $olddir to backup any existing dotfiles in ~"
+if [ -d "$olddir" ]; then
+	rm "$olddir"/*
+else
+	mkdir -p $olddir
+fi
+
 echo "...done!"
 
 #move any existing dotfile in homedir to dotfiles_old, then create symlinks
 
 for file in $files; do
 	echo "Moving old dotfiles from ~ to $olddir"
-	mv ~/.$file ~/.dotfiles_old/
+	mv ~/.$file $olddir
 	echo "creating symlinks to $file in home directory..."
 	ln -s $dir/$file ~/.$file
 done
